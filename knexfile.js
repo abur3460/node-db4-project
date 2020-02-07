66bc9306bc31046f9982788a7ae2c6d35288d3c7
Austin Burton <abur3460@gmail.com>
2020-02-06 17:13:49 -0800

    initial setup + file structure

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true, 
+    connection: {
+      filename: './data/database.db3',
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on foreign keys enforcement
+      },
+    },
+  },
+} 
